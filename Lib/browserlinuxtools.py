1a05eba889e1e766523308e18f6c8725c771d73e
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-02-20 20:56:25 +0000

    added blpm interfacing

--- Lib/browserlinuxtools.py
+++ Lib/browserlinuxtools.py
@@ -1,9 +1,35 @@
-from browser import window
+from browser import window as _window
+import os as _os
+import json as _json
 
-print = window.print
-color = window.color
-bold = window.bold
-tab = window.tab
-console = window.console
-clear = window.cmd_clear
+class blpm():
+  def __init__():
+    pass
+    
+  def install(packages):
+    _window.BLPM_INSTALL_QUEUE.extend(packages)
+
+  def remove(packages):
+    _window.cmd_blpm("remove "+" ".join(map(str, packages)))
+
+  def purge(packages):
+    _window.cmd_blpm("purge "+" ".join(map(str, packages)))
+
+  def list():
+    return _window.usr_bin.keys()
+
+  def listremote():
+    return _json.loads(os.open("/blpm-listall")).split(" ")
+
+  addCommandFromVMSH    = _window.addCommandFromVMSH
+  addCommandFromJS      = _window.addCommandFromJS
+  addCommandFromPython  = _window.addCommandFromJS
+  addCommandFromJSStr   = _window.addCommandFromJSStr
+
+print    = _window.print
+color    = _window.color
+bold     = _window.bold
+tab      = _window.tab
+console  = _window.console
+clear    = _window.cmd_clear
 
